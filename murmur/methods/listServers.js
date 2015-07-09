e336f70df6f420102c8eb6a0661bf960a504c4cf
Max Mikkola <max.mikkola@futurice.com>
2015-07-09 10:54:19 +0300

    List all servers instead of booted

--- murmur/methods/listServers.js
+++ murmur/methods/listServers.js
@@ -4,9 +4,7 @@ import Json from 'JSON'
 
 export default (req, res) => {
     meta.then((connection) => {
-        console.log('connected')
-        connection.getVersion().then((a,b,c,d) => console.log(a,b,c,d)) 
-        connection.getBootedServers().then((servers) => {
+        connection.getAllServers().then((servers) => {
             let promises = []
 
             servers.forEach((server, index) => {
@@ -24,11 +22,8 @@ function processServer (server) {
     return new Promise((resolve, reject) => {
         let serverPromises = [
             new Promise((resolve, reject) => server.id().then((id) => resolve({"id": id}))),
-            new Promise((resolve, reject) => server.isRunning().then((running) => resolve({"running": running}))),
-            new Promise((resolve, reject) => server.getUsers().then((users) => resolve({"users": users.size || 0})))
-            // new Promise((resolve, reject) => server.getDefaultConf().then((configMap) => {
-            //     resolve(configMap.keys)
-            // }))
+            new Promise((resolve, reject) => server.isRunning().then((running) => resolve({"running": running})))
+            // new Promise((resolve, reject) => server.getUsers().then((users) => resolve({"users": users.size || 0})))
         ]
         
         Promise.all(serverPromises).then((results)=> {
